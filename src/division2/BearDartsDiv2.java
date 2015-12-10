759840164cb92b188c58151413d9f55940b5a891
navid <navid@tubemogul.com>
2015-12-10 03:29:07 -0800

    2 more problems

---
+++ src/division2/BearDartsDiv2.java
@@ -0,0 +1,62 @@
+package division2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by navid.mazaheri on 12/10/15.
+ * https://community.topcoder.com/stat?c=problem_statement&pm=13479
+ * Possibilities to get a*b*c=d in sequential in natural order
+ */
+public class BearDartsDiv2 {
+    long count;
+
+    public static void main(String[] args) {
+        BearDartsDiv2 bearDartsDiv2 = new BearDartsDiv2();
+        System.out.println(bearDartsDiv2.count(new int[] { 10, 2, 2, 7, 40, 160 }));
+        System.out.println(bearDartsDiv2.count(new int[] { 128, 64, 32, 16, 8, 4, 2, 1 }));
+        System.out.println(bearDartsDiv2
+                .count(new int[] { 2, 3, 4, 5, 6, 8, 12, 16, 20, 24, 40, 24, 20, 16, 12, 8, 6, 5, 4, 3, 2 }));
+        System.out
+                .println(bearDartsDiv2.count(new int[] { 100, 100, 100, 1000000, 1000000, 1000000, 1000000, 1000000 }));
+        System.out.println(bearDartsDiv2
+                .count(new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }));
+    }
+
+    public long count(int[] w) {
+        count = 0;
+        List<Integer> list = new ArrayList();
+        for (int i = 0; i < w.length; i++) {
+            list.add(w[i]);
+            if (list.size() > 3)
+                calcPermutations(list, list.get(list.size() - 1));
+        }
+        return count;
+    }
+
+    private void calcPermutations(List<Integer> list, int sum) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = i + 1; j < list.size() - 1; j++) {
+                for (int k = j + 1; k < list.size() - 1; k++) {
+                    if (list.get(i) * list.get(j) * list.get(k) == sum)
+                        count++;
+                }
+            }
+        }
+    }
+}
